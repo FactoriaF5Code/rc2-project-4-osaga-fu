a03472b6b00d3ac968cd4d6ec007cabf600cb410
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2023-12-20 15:03:22 +0100

    Solucionado error en el return del header y footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,15 @@ import footer02 from "../assets/footer02.png";
 import footer03 from "../assets/footer03.png";
 
 export default function Footer() {
-  <footer>
-    <img src={footer01} alt="Union Europea" className="imagenFooter" />
-    <img src={footer02} alt="Gobierno de España" className="imagenFooter" />
-    <img
-      src={footer03}
-      alt="Generalitat de Catalunya"
-      className="imagenFooter"
-    />
-  </footer>
+  return (
+    <footer>
+      <img src={footer01} alt="Union Europea" className="imagenFooter" />
+      <img src={footer02} alt="Gobierno de España" className="imagenFooter" />
+      <img
+        src={footer03}
+        alt="Generalitat de Catalunya"
+        className="imagenFooter"
+      />
+    </footer>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,17 @@ import "./Header.css";
 import logo from "../assets/filminLogo.png";
 
 export default function Header() {
-  <header>
-    <nav>
-      <img src={logo} alt="Logo Filmin" className="logo" />
-      <h3>Inicio</h3>
-      <h3>Películas</h3>
-    </nav>
-    <section className="buttonsContainer">
-      <button className="buttonSubscribe">Suscríbete</button>
-      <button className="buttonLogin">Iniciar Sesión</button>
-    </section>
-  </header>
+  return (
+    <header>
+      <nav>
+        <img src={logo} alt="Logo Filmin" className="logo" />
+        <h3>Inicio</h3>
+        <h3>Películas</h3>
+      </nav>
+      <section className="buttonsContainer">
+        <button className="buttonSubscribe">Suscríbete</button>
+        <button className="buttonLogin">Iniciar Sesión</button>
+      </section>
+    </header>
+  );
 }
